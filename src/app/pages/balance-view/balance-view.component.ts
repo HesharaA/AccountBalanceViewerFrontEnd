f86be62b4182ef68c4d5f7ebfe835606c4027369
Heshara@99x <151610769+HesharaA@users.noreply.github.com>
2024-06-17 19:00:14 +0530

    Implement basic styles in upload page

--- src/app/pages/balance-view/balance-view.component.ts
+++ src/app/pages/balance-view/balance-view.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Balance, BalanceService } from '../../services/balance.service';
 import { NgFor } from '@angular/common';
+import { CardComponent } from '../../components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-balance-view',
   standalone: true,
-  imports: [NgFor],
   templateUrl: './balance-view.component.html',
   styleUrl: './balance-view.component.css',
+  imports: [NgFor, CardComponent, CommonModule],
 })
 export class BalanceViewComponent implements OnInit {
   balances: Balance[] = [];
