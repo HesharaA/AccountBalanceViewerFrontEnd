e9509507376480e0bc7bb92a4cb5c85372656334
Heshara@99x <151610769+HesharaA@users.noreply.github.com>
2024-06-17 15:56:33 +0530

    Implement upload screen basic styling

--- src/app/pages/balance-upload/balance-upload.component.ts
+++ src/app/pages/balance-upload/balance-upload.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
 import { BalanceService } from '../../services/balance.service';
+import { LoaderComponent } from '../../components/loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-balance-upload',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent, CommonModule, CardComponent],
   templateUrl: './balance-upload.component.html',
   styleUrl: './balance-upload.component.css',
 })
 export class BalanceUploadComponent {
   selectedFile: any;
+  isUploadButtonDisabled: boolean = true;
+  showLoading: boolean = false;
 
   constructor(private balanceService: BalanceService) {}
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
+    if (this.selectedFile !== null || this.selectedFile !== undefined) {
+      this.isUploadButtonDisabled = false;
+    }
   }
 
   onUpload(): void {
+    this.showLoading = true;
     if (this.selectedFile) {
       this.balanceService
         .uploadBalanceFile(this.selectedFile)
         .subscribe((response) => {
+          this.showLoading = false;
           console.log('File uploaded successfully', response);
         });
     }
