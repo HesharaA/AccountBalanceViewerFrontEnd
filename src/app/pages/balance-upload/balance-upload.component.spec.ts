30839626bda4c94d1d37f2da84d4bfddec947a07
Heshara@99x <151610769+HesharaA@users.noreply.github.com>
2024-06-18 22:23:08 +0530

    Implemet upload view tests

--- src/app/pages/balance-upload/balance-upload.component.spec.ts
+++ src/app/pages/balance-upload/balance-upload.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BalanceUploadComponent } from './balance-upload.component';
+import { BalanceViewComponent } from '../balance-view/balance-view.component';
+import { Router, provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+import { fakeAsync } from '@angular/core/testing';
+import { tick } from '@angular/core/testing';
 
 describe('BalanceUploadComponent', () => {
   let component: BalanceUploadComponent;
@@ -8,9 +14,13 @@ describe('BalanceUploadComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BalanceUploadComponent]
-    })
-    .compileComponents();
+      imports: [BalanceUploadComponent],
+      providers: [
+        provideRouter([{ path: '**', component: BalanceViewComponent }]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BalanceUploadComponent);
     component = fixture.componentInstance;
@@ -20,4 +30,53 @@ describe('BalanceUploadComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should select file and upload', fakeAsync(() => {
+    spyOn(component, 'onUpload');
+
+    const testingFile = new File([''], 'test-file.txt');
+
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(testingFile);
+
+    const fileInput = fixture.debugElement.query(By.css('input[type=file]'));
+    fileInput.nativeElement.files = dataTransfer.files;
+    fileInput.nativeElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+
+    let uploadButton = fixture.debugElement.nativeElement.querySelector(
+      'button[type=submit]'
+    );
+    uploadButton.click();
+    tick();
+
+    expect(component.onUpload).toHaveBeenCalled();
+  }));
+
+  it('should click upload without file', fakeAsync(() => {
+    spyOn(component, 'onUpload');
+
+    let uploadButton = fixture.debugElement.nativeElement.querySelector(
+      'button[type=submit]'
+    );
+    uploadButton.click();
+    tick();
+
+    expect(component.onUpload).not.toHaveBeenCalled();
+  }));
+
+  it('should click on available balance', fakeAsync(() => {
+    component.availableBalanceDates = [
+      new Date('2023-03-01T00:00:00'),
+      new Date('2024-03-31T00:00:00'),
+    ];
+    fixture.detectChanges();
+
+    let availableBalanceButton =
+      fixture.debugElement.nativeElement.querySelector('a');
+    availableBalanceButton.click();
+    tick();
+
+    expect(TestBed.inject(Router).url).toContain('/balances?date');
+  }));
 });
